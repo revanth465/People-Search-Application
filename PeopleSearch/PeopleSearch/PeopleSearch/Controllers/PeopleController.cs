f561837167302dafcfe8a66c877733f0624b664c
revanth465 <rmalay@uncc.edu>
2016-01-24 12:56:30 -0800

    People Search Application

---
+++ PeopleSearch/PeopleSearch/PeopleSearch/Controllers/PeopleController.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PeopleSearch.Models;
+
+namespace PeopleSearch.Controllers
+{
+	public class PeopleController : Controller
+	{
+		private PeopleDBContext db = new PeopleDBContext();
+       public void setContext(DbSet<Person> obj)
+       {
+           db.People = obj;
+       }
+		[HttpPost]
+		public string Index(FormCollection fc, string firstName)
+		{
+            return "<h3> From [HttpPost]Index: " + firstName + "</h3>";
+		}
+
+		// GET: People Info displays
+        public ActionResult Index()
+		{
+			var people = from m in db.People
+						 select m;
+            
+			return View(people);
+		}
+
+        [HttpPost]
+	    public ActionResult Search(string firstName, string lastName)
+	    {
+            if (string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName))
+            {
+                return null;
+            }
+
+            var people = (from m in db.People
+                         where m.LastName.Equals(lastName.ToLower()) || m.FirstName.Equals(firstName.ToLower())
+                         select m).ToList();
+
+            if (!people.Any())
+            {
+                return null;
+            }
+
+            return View(people);
+	    }
+
+		// GET: People/Details/5
+		public ActionResult Details(int? id)
+		{
+			if (id == null)
+			{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
+			Person person = db.People.Find(id);
+			if (person == null)
+			{
+				return HttpNotFound();
+			}
+			return View(person);
+		}
+
+		// GET: People/Create
+		public ActionResult Create()
+		{
+			return View();
+		}
+
+		// POST: People/Create
+		// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+		// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Create([Bind(Include = "PersonID,FirstName,LastName,Address,Age,Interests,Picture,AlternateText")] Person person)
+		{
+			if (ModelState.IsValid)
+			{
+				db.People.Add(person);
+				db.SaveChanges();
+				return RedirectToAction("Index");
+			}
+
+			return View(person);
+		}
+
+		// GET: People/Edit/5
+		public ActionResult Edit(int? id)
+		{
+			if (id == null)
+			{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
+			Person person = db.People.Find(id);
+			if (person == null)
+			{
+				return HttpNotFound();
+			}
+			return View(person);
+		}
+
+		// POST: People/Edit/5
+		// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+		// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Edit([Bind(Include = "PersonID,FirstName,LastName,Address,Age,Interests,Picture,AlternateText")] Person person)
+		{
+			if (ModelState.IsValid)
+			{
+				db.Entry(person).State = EntityState.Modified;
+				db.SaveChanges();
+				return RedirectToAction("Index");
+			}
+			return View(person);
+		}
+
+		// GET: People/Delete/5
+		public ActionResult Delete(int? id)
+		{
+			if (id == null)
+			{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
+			Person person = db.People.Find(id);
+			if (person == null)
+			{
+				return HttpNotFound();
+			}
+			return View(person);
+		}
+
+		// POST: People/Delete/5
+		[HttpPost, ActionName("Delete")]
+		[ValidateAntiForgeryToken]
+		public ActionResult DeleteConfirmed(int id)
+		{
+			Person person = db.People.Find(id);
+			db.People.Remove(person);
+			db.SaveChanges();
+			return RedirectToAction("Index");
+		}
+
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing)
+			{
+				db.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+	}
+}
---
+++ PeopleSearch/PeopleSearch/PeoplesearchTest/UnitTest1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using Moq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using PeopleSearch.Controllers;
+using System.Web.Mvc;
+using PeopleSearch.Models;
+using System.Data.Entity;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace PeoplesearchTest
+{
+   
+
+    [TestClass]
+    public class UnitTest1 
+    {
+        PeopleController pc = null;
+        
+        public UnitTest1()
+        {
+            var data = new List<Person>
+            {
+                new Person(){ FirstName="Alex", LastName="Luka"},
+                new Person(){ FirstName="Johnn", LastName="Depp"},
+                new Person(){ FirstName="Christian", LastName="Christian"},
+                new Person(){ FirstName="Kevin", LastName="Spacey"}
+            }.AsQueryable();
+
+            var mockSet = new Mock<DbSet<Person>>();
+            mockSet.As<IQueryable<Person>>().Setup(m => m.Provider).Returns(data.Provider); 
+            mockSet.As<IQueryable<Person>>().Setup(m => m.Expression).Returns(data.Expression); 
+            mockSet.As<IQueryable<Person>>().Setup(m => m.ElementType).Returns(data.ElementType); 
+            mockSet.As<IQueryable<Person>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
+            Mock<PeopleDBContext> myMock = new Mock<PeopleDBContext>();
+            myMock.Setup(m => m.People).Returns(mockSet.Object);
+            pc= new PeopleController();
+            pc.setContext(mockSet.Object);
+        }
+        // Testing for Edit Method of People Controller...
+        [TestMethod]
+        public void editTest()
+        {
+           
+            ActionResult result = pc.Edit(1);
+          // NUnit.Framework.Assert.IsInstanceOf(result.GetType(), typeof(ViewResult));
+            var vResult = result as ViewResult;
+            var prn = new Person();
+            prn.FirstName = "Bob";
+            prn.LastName = "Nelson";            
+            if (vResult != null)
+            {
+               // Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsInstanceOfType(vResult.Model, typeof(PeopleSearch.Models.Person));
+                var model = vResult.Model as Person;
+                if (model != null)
+                {
+                    Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(prn.FirstName, model.FirstName);
+                    Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(prn.LastName, model.LastName);
+                    Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(prn.Address, model.Address); 
+                }
+
+            }
+        }
+        // Testing for details method of People Controller...
+        [TestMethod]
+        public void detailsTest()
+        {
+
+            ActionResult result = pc.Details(1);
+            // NUnit.Framework.Assert.IsInstanceOf(result.GetType(), typeof(ViewResult));
+            ViewResult vResult = result as ViewResult;
+            var prn = new Person();
+            prn.FirstName = "Alex";
+            prn.LastName = "Luka";
+            if (vResult != null)
+            {
+                // Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsInstanceOfType(vResult.Model, typeof(PeopleSearch.Models.Person));
+                var model = vResult.Model as Person;
+                if (model != null)
+                {
+                    Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsNotNull(prn.FirstName);
+                    Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(prn.FirstName, model.FirstName);
+                    Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(prn.LastName, model.LastName);
+                  //  Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(prn.Address, model.Address); 
+                }
+               
+            }
+        }
+
+        [TestCase("Alex", "luka")]
+        [TestCase("John","Depp")]
+        [TestCase("Christian","Bale")]
+        public void PeopleController_Search_NotNull(string firstName, string lastName)
+        {
+            var peopleController = new PeopleController();
+
+            var result = peopleController.Search(firstName, lastName);
+
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsNotNull(result);
+        }
+
+
+        [TestCase("fake", "user1")]
+        [TestCase("fake", "user2")]
+        [TestCase("fake", "user3")]
+        public void PeopleController_Search_Null(string firstName, string lastName)
+        {
+            var peopleController = new PeopleController();
+
+            var result = peopleController.Search(firstName, lastName);
+
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsNull(result);
+        }
+
+
+    }
+}
